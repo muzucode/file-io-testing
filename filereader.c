0fc8c31c18b02e26b0392ea26b39c54b5f50ad9a
Muzucode <seanhickey4516@gmail.com>
2024-06-15 10:31:05 -0400

    updates

--- filereader.c
+++ filereader.c
@@ -4,6 +4,39 @@
 #include <unistd.h>
 
 
+char* readFile(char* pathname) {
+    FILE* file;
+    char* contents;
+    long fileSize;
+
+
+    file = fopen(pathname, "r");
+
+    fseek(file, 0, SEEK_END);
+    fileSize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    contents = (char*)malloc(fileSize + 1);
+    size_t bytesRead = fread(contents, sizeof(char), fileSize, file);
+
+    contents[bytesRead+1] = '\0';
+
+    fclose(file);
+
+    return contents;
+    
+}
+
+
 int main() {
+    char* filecontents;
+
+    // Read the file!
+    filecontents = readFile("files-to-download.txt");
+    
+    printf("My file contents: \n");
+    printf("%s\n", filecontents);
+
+    free(filecontents);
     return 0;
 }
